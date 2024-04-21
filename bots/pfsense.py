987f259f0c5b41944166e1616132c64503d5f0e7
jayneykim <gim515@naver.com>
2024-04-21 14:11:25 +0900

    go secure

--- bots/pfsense.py
+++ bots/pfsense.py
@@ -1,9 +1,13 @@
 from flask import request
 from flask_restx import Resource, Api, Namespace 
 Pfsenses = Namespace('pfsense')
+import os
 
 @Pfsenses.route('') 
 class SetIPAddr(Resource):
-    def get(self): 
-        return request.args.get("text")
+    def get(self):  
+        if request.args.get("secret") == os.getenv("BOT_PFSENSE_SECRET"):
+            return request.args.get("text")
+        else:
+            return 403
  
